3e46a164c6c69cea36b7dfcc5fccc7a9c5090c57
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-05 11:35:04 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,24 +1,18 @@
 <?php
-// used to connect to the database
-$host = "localhost";
-$db_name = "1phpbeginnercrudlevel1";
-$username = "root";
-$password = "";
- 
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-}
- 
-// show error
-catch(PDOException $exception){
-    echo "Connection error: " . $exception->getMessage();
-}
-
-$action = isset($_GET['action']) ? $_GET['action'] : "";
- 
-// if it was redirected from delete.php
-if($action=='deleted'){
-    echo "<div class='alert alert-success'>Record was deleted.</div>";
-}
+	//print_r(PDO::getAvailableDrivers());
+	$dbname='yejianchen';	//forcatoalb
+	$dbusername='webdb';	//webdb
+	$dbpassword='webdb';		//webdb
+	
+	// Connessione al database
+	//Nuova istanza della classe PDO
+	try{
+		$db=new PDO("pgsql:dbname=$dbname",$dbusername,$dbpassword);
+		//Se ci sono errori li scrive
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//echo "Connessione col DB OK";
+	}
+	catch (PDOException $e) {
+		echo "Errore". $e->getMessage();
+	}
 ?>
-
