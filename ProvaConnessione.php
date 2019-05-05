b490e95ee7ba10bd74dba61259445f53c51682f5
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-05 11:57:12 +0200

    Create ProvaConnessione.php

---
+++ ProvaConnessione.php
@@ -0,0 +1,22 @@
+<?php
+	//print_r(PDO::getAvailableDrivers());
+	$dbname='yejianchen';	//forcatoalb
+	$dbusername='webdb';	//webdb
+	$dbpassword='webdb';		//webdb
+	
+	// Connessione al database
+	//Nuova istanza della classe PDO
+	try{
+		$db=new PDO("pgsql:dbname=$dbname",$dbusername,$dbpassword);
+		//Se ci sono errori li scrive
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connessione col DB OK";
+    
+    
+    
+    
+    }
+	catch (PDOException $e) {
+		echo "Errore". $e->getMessage();
+	}
+?>
