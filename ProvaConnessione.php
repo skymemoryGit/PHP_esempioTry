b07d1bedfdd02a136e4f8d0cd7c4d6cb5c9ab4b4
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-06 14:11:29 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -10,7 +10,7 @@
 		$db=new PDO("pgsql:dbname=$dbname",$dbusername,$dbpassword);
 		//Se ci sono errori li scrive
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		//echo "Connessione col DB OK";
+		echo "Connessione col DB OK";
     
     
     }
@@ -19,73 +19,8 @@
     }
 
     
-    /*$prova= $db->query("SELECT matricola FROM studente");
-    echo prova;
-
-    while($row = $prova->fetch()){
-        <?php echo $row["matricola"]; ?>
-    }*/
-
-     //select all data php 
-     $query = "SELECT nome, cognome, matricola FROM studente";
-     $stmt = $db-> prepare( $query );
-     $stmt->execute();
-   
-     // this is how to get number of rows returned
-     $num = $stmt->rowCount();
-   
- //  check if more than 0 record found
- 
-     echo "porco D";
-     print $num;
-     if($num>0){
-   
-     //start table
-     echo "<table border='1'>";
-   
-         //creating our table heading
-         echo "<tr>";
-             echo "<th>1</th>";
-             echo "<th>2</th>";
-             echo "<th>3</th>";
-             
-         echo "</tr>";
-   
-         //retrieve our table contents
-         //fetch() is faster than fetchAll()
-         //http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
- 
-         
-         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-             //extract row
-             //this will make $row['firstname'] to
-             //just $firstname only
-             extract($row);
-   
-             //creating new table row per record
-             echo "<tr>";
-                 echo "<td>{$nome}</td>";
-                 echo "<td>{$cognome}</td>";
-                 echo "<td>{$matricola}</td>";
-             
-                     //we will use this links on next part of this post
-                     echo "<a href='edit.php?id={$id}'>Edit</a>";
-                     echo " / ";
-                     //we will use this links on next part of this post
-                     echo "<a href='#' onclick='delete_user( {$id} );'>Delete</a>";
-                 echo "</td>";
-             echo "</tr>";
-         }
-   
-     //end table
-     echo "</table>";
-   
- }
-   
- //if no records found
- else{
-     echo "No records found. passato da num==0";
- }
+    //$prova= $db->query("SELECT matricola FROM studente");
+    
 
     
 
