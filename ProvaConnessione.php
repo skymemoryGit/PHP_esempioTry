452a421c0334140ac73ebdac886fe28fef3fa2ca
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:57:43 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -1,8 +1,8 @@
 <?php
 	//print_r(PDO::getAvailableDrivers());
-	$dbname='yejianchen';	//forcatoalb
-	$dbusername='webdb';	//webdb
-	$dbpassword='webdb';		//webdb
+	$dbname='yejianchen';
+	$dbusername='webdb';	
+	$dbpassword='webdb';		
 	
 	// Connessione al database
 	//Nuova istanza della classe PDO
@@ -22,50 +22,20 @@
 
 		echo "dopo catch";
 
-		$lstStand=$db->query("SELECT * from studente;");
+		$sql=$db->query("SELECT * FROM studente;");
+		echo "$sql";
+
+		
+ 
+ 
+ 
+ 
  ?>
 
- <html>
-	<body>
-		<div style="text-align:center;">
-			<img src="img/reUNIonPD.png" /><br/>
-			<fieldset>			
-			<?php
-				while($stand=$lstStand->fetch()){
-					$sql=$db->query("SELECT nome, cognome FROM studente;"); ?>
-
-					<table class="vis" width="75%" border="1" align="center" >
-					<caption><h2>VOLONTARI STAND "<?php echo $stand["nome"]; ?>"</h2></caption>
-					<?php
-					$volo=$sql->fetch();
-					if(!isset($volo["nome"]))
-							echo "<tr><td>Nessun volontario ancora inserito</td></tr></table>";
-					else{
-						?> 
-						<tr><th>Nome</th><th>Cognome</th></tr>
-						<?php				
-						do {
-								echo "<tr><td>".$volo["nome"]."</td><td>".$volo["cognome"]."</td></tr>";
-						}while($volo=$sql->fetch());
-						echo "</table>";
-					}
-		    	}
-			?>
-			</fieldset>
-			<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
-		</div>
-		
-	</body>
-</html>
-
-
-<?php
-		
-		echo "_____dopo istruzione";
+ 
 
 	
 
 
 		
 
-?>
