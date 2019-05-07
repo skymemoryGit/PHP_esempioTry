da2737d8375e468414b66f4fd33f3c696919f077
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:29:15 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -18,35 +18,7 @@
 		echo "Errore". $e->getMessage();
     }
 
-		<?php
-    ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL | E_STRICT);
-    //include("header.php");
-    $sql=$db->query("SELECT * FROM stand;");
-    $sql->setFetchMode(PDO::FETCH_BOTH);
- ?>
- <html>
-	<body>
-		<div style="text-align:center;">
-			//<img src="img/reUNIonPD.png" /><br/>
-			<fieldset>
-			<table class="vis" width="75%" border="1" align="center">
-			<caption><h2>LISTA STAND PRESENTI</h2></caption>
-			<tr><th>Nome</th><th>Descrizione</th><th>Responsabile</th>
-			<?php
-				while($row=$sql->fetch()){
-						$lstStand=$row;
-				  echo '<tr><td>'.$row['nome'].'</td><td>'.$row['descrizione'].'</td><td>'.$row['studente'].'</td>';
-					}
-			?>
-			</table>
-			</fieldset>
-			<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
-		</div>
-		
-	</body>
-</html>
+	
 
 
 		
