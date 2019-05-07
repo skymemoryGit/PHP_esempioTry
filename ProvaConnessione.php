4525f8136117cb7e79e0a6a5891c70fe46e4f458
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:26:22 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -1,6 +1,6 @@
 <?php
 	//print_r(PDO::getAvailableDrivers());
-	$dbname='yejianchen';	
+	$dbname='forcatoalb';	//forcatoalb
 	$dbusername='webdb';	//webdb
 	$dbpassword='webdb';		//webdb
 	
@@ -18,43 +18,37 @@
 		echo "Errore". $e->getMessage();
     }
 
-		
-		/*
-		$prova= $db->query("SELECT matricola FROM studente");
-		echo 	$prova;
-    echo "passato";
-    */
-
-
 		<?php
     ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
+	ini_set('display_startup_errors', 1);
     error_reporting(E_ALL | E_STRICT);
-    
-    $sql=$db->query("SELECT * FROM studente;");
+    //include("header.php");
+    $sql=$db->query("SELECT * FROM stand;");
     $sql->setFetchMode(PDO::FETCH_BOTH);
-		 ?>
-		 
-		 <html>
-		 <body>
-			 <div style="text-align:center;">
-				 //<img src="img/reUNIonPD.png" /><br/>
-				 <fieldset>
-				 <table class="vis" width="75%" border="1" align="center">
-				 <caption><h2>LISTA STAND PRESENTI</h2></caption>
-				 <tr><th>Nome</th><th>Descrizione</th><th>Responsabile</th>
-				 <?php
-					 while($row=$sql->fetch()){
-							 $lstStand=$row;
-						 echo '<tr><td>'.$row['matricola'].'</td><td>'.$row['nome'].'</td><td>'.$row['cognome'].'</td>';
-						 }
-				 ?>
-				 </table>
-				 </fieldset>
-				 //<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
-			 </div>
-			 
-		 </body>
-	 </html>
+ ?>
+ <html>
+	<body>
+		<div style="text-align:center;">
+			//<img src="img/reUNIonPD.png" /><br/>
+			<fieldset>
+			<table class="vis" width="75%" border="1" align="center">
+			<caption><h2>LISTA STAND PRESENTI</h2></caption>
+			<tr><th>Nome</th><th>Descrizione</th><th>Responsabile</th>
+			<?php
+				while($row=$sql->fetch()){
+						$lstStand=$row;
+				  echo '<tr><td>'.$row['nome'].'</td><td>'.$row['descrizione'].'</td><td>'.$row['studente'].'</td>';
+					}
+			?>
+			</table>
+			</fieldset>
+			<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
+		</div>
+		
+	</body>
+</html>
+
+
+		
 
 ?>
