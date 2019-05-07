403bf893942c76f63e88efb8796109b6f49fd712
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:46:23 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -22,9 +22,44 @@
 
 		echo "dopo catch";
 
+		$lstStand=$db->query("SELECT * from studente;");
+ ?>
+
+ <html>
+	<body>
+		<div style="text-align:center;">
+			<img src="img/reUNIonPD.png" /><br/>
+			<fieldset>			
+			<?php
+				while($stand=$lstStand->fetch()){
+					$sql=$db->query("SELECT nome, cognome FROM studente;"); ?>
+
+					<table class="vis" width="75%" border="1" align="center" >
+					<caption><h2>VOLONTARI STAND "<?php echo $stand["nome"]; ?>"</h2></caption>
+					<?php
+					$volo=$sql->fetch();
+					if(!isset($volo["nome"]))
+							echo "<tr><td>Nessun volontario ancora inserito</td></tr></table>";
+					else{
+						?> 
+						<tr><th>Nome</th><th>Cognome</th></tr>
+						<?php				
+						do {
+								echo "<tr><td>".$volo["nome"]."</td><td>".$volo["cognome"]."</td></tr>";
+						}while($volo=$sql->fetch());
+						echo "</table>";
+					}
+		    	}
+			?>
+			</fieldset>
+			<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
+		</div>
 		
-		$sql=$db->query("SELECT * FROM studente;");
-		//$sql->setFetchMode(PDO::FETCH_BOTH);
+	</body>
+</html>
+
+
+<?php
 		
 		echo "_____dopo istruzione";
 
