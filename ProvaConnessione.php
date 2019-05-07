c07abd194da6f4cbdbd0bb232f70069898e3e498
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:34:36 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -1,6 +1,6 @@
 <?php
 	//print_r(PDO::getAvailableDrivers());
-	$dbname='forcatoalb';	//forcatoalb
+	$dbname='yejianchen';	//forcatoalb
 	$dbusername='webdb';	//webdb
 	$dbpassword='webdb';		//webdb
 	
@@ -11,12 +11,21 @@
 		//Se ci sono errori li scrive
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		echo "Connessione stronzo col DB OK";
+		echo "passo dopo la stringa connessione";
     
     
     }
 	catch (PDOException $e) {
 		echo "Errore". $e->getMessage();
-    }
+		}
+		
+
+		echo "dopo catch";
+
+		/*
+		$sql=$db->query("SELECT * FROM studente;");
+		$sql->setFetchMode(PDO::FETCH_BOTH);
+		/*	
 
 	
 
