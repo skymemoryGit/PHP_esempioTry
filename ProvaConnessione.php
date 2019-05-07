ecf2d1e49f9ddb15c2140b04bfe70dcee61bd5a4
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2019-05-07 11:22:36 +0200

    Update ProvaConnessione.php

--- ProvaConnessione.php
+++ ProvaConnessione.php
@@ -31,18 +31,30 @@
 		ini_set('display_startup_errors', 1);
     error_reporting(E_ALL | E_STRICT);
     
-    $sql=$db->query("SELECT * FROM studenti;");
+    $sql=$db->query("SELECT * FROM studente;");
     $sql->setFetchMode(PDO::FETCH_BOTH);
 		 ?>
 		 
-
-		 <table class="vis" width="75%" border="1" align="center">
-		 <?php
-				while($row=$sql->fetch()){
-						$lstStand=$row;
-				  echo '<tr><td>'.$row['matricola'].'</td><td>'.$row['nome'].'</td><td>'.$row['cognome'].'</td>';
-					}
-			?>
-    </table>
+		 <html>
+		 <body>
+			 <div style="text-align:center;">
+				 //<img src="img/reUNIonPD.png" /><br/>
+				 <fieldset>
+				 <table class="vis" width="75%" border="1" align="center">
+				 <caption><h2>LISTA STAND PRESENTI</h2></caption>
+				 <tr><th>Nome</th><th>Descrizione</th><th>Responsabile</th>
+				 <?php
+					 while($row=$sql->fetch()){
+							 $lstStand=$row;
+						 echo '<tr><td>'.$row['matricola'].'</td><td>'.$row['nome'].'</td><td>'.$row['cognome'].'</td>';
+						 }
+				 ?>
+				 </table>
+				 </fieldset>
+				 //<br/><a href="index.php"><input type="button" value="TORNA INDIETRO"/></a><br/>
+			 </div>
+			 
+		 </body>
+	 </html>
 
 ?>
